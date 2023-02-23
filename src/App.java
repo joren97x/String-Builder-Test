3e80d68eec5075d2b2c15dcdbd15cbc803510291
joren97x <babamyloves2002@gmail.com>
2023-02-22 19:20:02 -0800

    trying to reverse an int using string builder 1

--- src/App.java
+++ src/App.java
@@ -1,28 +1,47 @@
+import java.util.Scanner;
+
+import javax.sound.sampled.ReverbType;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        StringBuilder hello = new StringBuilder("Hello");
-        System.out.println(hello);
+        // StringBuilder hello = new StringBuilder("Hello");
+        // System.out.println(hello);
 
-        hello.append(" World!");
-        System.out.println(hello);
+        // hello.append(" World!");
+        // System.out.println(hello);
 
-        hello.reverse();
-        System.out.println(hello);
+        // hello.reverse();
+        // System.out.println(hello);
 
-        hello.reverse();
-        System.out.println(hello);
+        // hello.reverse();
+        // System.out.println(hello);
 
-        hello.delete(6, 12);
-        System.out.println(hello);
+        // hello.delete(6, 12);
+        // System.out.println(hello);
 
-        hello.insert(6, "Joren!");
-        System.out.println(hello);
+        // hello.insert(6, "Joren!");
+        // System.out.println(hello);
 
-        hello.insert(6, "world and ");
-        System.out.println(hello);
+        // hello.insert(6, "world and ");
+        // System.out.println(hello);
 
-        hello.replace(6, 22, "World!");
-        System.out.println(hello);
+        // hello.replace(6, 22, "World!");
+        // System.out.println(hello);
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        reverserInt(x);
+
+        sc.close();
 
     }
+
+    public int reverseInt(int x) {
+
+        
+
+        return 0;
+    }
+
 }
