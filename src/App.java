b504ccbdcbab3c3ad768240e02a910b4a1352acb
joren97x <babamyloves2002@gmail.com>
2023-02-22 19:07:38 -0800

    string builder is faster than string buffer, string buffer is slower but safer in threads

---
+++ src/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        StringBuilder hello = new StringBuilder("Hello");
+        System.out.println(hello);
+
+        hello.append(" World!");
+        System.out.println(hello);
+
+        hello.reverse();
+        System.out.println(hello);
+
+        hello.reverse();
+        System.out.println(hello);
+
+        hello.delete(6, 12);
+        System.out.println(hello);
+
+        hello.insert(6, "Joren!");
+        System.out.println(hello);
+
+        hello.insert(6, "world and ");
+        System.out.println(hello);
+
+        hello.replace(6, 22, "Giatay!");
+        System.out.println(hello);
+
+    }
+}
